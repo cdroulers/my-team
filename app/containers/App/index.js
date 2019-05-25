b80a9c86e75f70b9077f75dcd2bda4dfa9eadb20
Christian Droulers <droulers.christian@gmail.com>
2019-05-25 00:17:51 -0400

    cleanup v1

---
+++ app/containers/App/index.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * App.js
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
+ */
+
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import HomePage from "containers/HomePage/Loadable";
+import NotFoundPage from "containers/NotFoundPage/Loadable";
+
+import GlobalStyle from "../../global-styles";
+
+function Test() {
+  return <div>hai, this is a test page. lkjsdfjklsdf</div>;
+}
+
+export default function App() {
+  return (
+    <div>
+      TEST
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/test" component={Test} />
+        <Route component={NotFoundPage} />
+      </Switch>
+      <GlobalStyle />
+    </div>
+  );
+}
---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,20 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ *
+ */
+
+import React from "react";
+import { FormattedMessage } from "react-intl";
+import { Link } from "react-router-dom";
+import messages from "./messages";
+
+export default function HomePage() {
+  return (
+    <h1>
+      <FormattedMessage {...messages.header} />
+      <Link to="/test">hai</Link>
+    </h1>
+  );
+}
